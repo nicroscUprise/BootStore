318c6d7f14d99d08d0f311b2776b89ba71f3ee8b
Niko Zagar <niko.zagar@uprise.hr>
2022-10-10 13:15:16 +0200

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+function App() {
+  const [darkMode, setDarkmode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+  
+function handleThemeChange(){
+  setDarkmode(!darkMode);
+}
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
+        <Catalog/>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
